ea86386e63d55eb7910b2b0390cd8b1b90b57d79
Sumona18 <84658326+Sumona18@users.noreply.github.com>
2022-03-23 12:18:20 +0530

    commit message

--- src/components/status.js
+++ src/components/status.js
@@ -20,7 +20,7 @@ import {Row, Col, Container} from 'react-bootstrap';
 </Container>
 
 <h2>OATH</h2>
-<p style={{margin: 50}}> Bros before hoes, neever leave a bro hangin' and thy shall always tell when thy had sex. Uderstood? Should be available whenever thy need and thy clear all the doubts under the ability.Uderstood?Whenever she calls to visit her college, must listen and make plans for the same at its earliest.Uderstood?</p>
+<p style={{margin: 50}}> Bros before hoes, never leave a bro hangin' and thy shall always tell when thy had sex. Understood? Should be available whenever thy need and thy clear all the doubts under the ability.Understood? Whenever she calls to visit her college, must listen and make plans for the same at its earliest. Understood?</p>
 <br/><br/>
 <h4>Happy Birthday!</h4> <br />
            
